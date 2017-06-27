cab6406a88a9e0a0ac31a88fe5335413825a65dc
Ishan Vashishta <ivashi@umich.edu>
2017-06-27 16:46:50 -0400

    community draft, new logo

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,71 @@
+function changeTimeline($idNum){
+  clearTimeline();
+
+  switch($idNum){
+    case 1:
+      document.getElementById("timeline1").style.display = 'block';
+      break;
+    case 2:
+      document.getElementById("timeline2").style.display = 'block';
+      break;
+    case 3:
+      document.getElementById("timeline3").style.display = 'block';
+      break;
+    case 4:
+      document.getElementById("timeline4").style.display = 'block';
+      break;
+    case 5:
+      document.getElementById("timeline5").style.display = 'block';
+      break;
+    case 6:
+      document.getElementById("timeline6").style.display = 'block';
+      break;
+    case 7:
+      document.getElementById("timeline7").style.display = 'block';
+      break;
+    case 8:
+      document.getElementById("timeline8").style.display = 'block';
+      break;
+    case 9:
+      document.getElementById("timeline9").style.display = 'block';
+      break;
+    case 10:
+      document.getElementById("timeline10").style.display = 'block';
+      break;
+    case 11:
+      document.getElementById("timeline11").style.display = 'block';
+      break;
+    case 12:
+      document.getElementById("timeline12").style.display = 'block';
+      break;
+    case 13:
+      document.getElementById("timeline13").style.display = 'block';
+      break;
+    case 14:
+      document.getElementById("timeline14").style.display = 'block';
+      break;
+  }
+  return;
+}
+
+function clearTimeline(){
+  document.getElementById("timeline1").style.display = 'none';
+  document.getElementById("timeline2").style.display = 'none';
+  document.getElementById("timeline3").style.display = 'none';
+  document.getElementById("timeline4").style.display = 'none';
+  document.getElementById("timeline5").style.display = 'none';
+  document.getElementById("timeline6").style.display = 'none';
+  document.getElementById("timeline7").style.display = 'none';
+  document.getElementById("timeline8").style.display = 'none';
+  document.getElementById("timeline9").style.display = 'none';
+  document.getElementById("timeline10").style.display = 'none';
+  document.getElementById("timeline11").style.display = 'none';
+  document.getElementById("timeline12").style.display = 'none';
+  document.getElementById("timeline13").style.display = 'none';
+  document.getElementById("timeline14").style.display = 'none';
+}
+
+
 function changeCollab($collab_id){
     document.getElementById("first").style.display = 'none';
     document.getElementById("second").style.display = 'none';
@@ -5,7 +73,6 @@ function changeCollab($collab_id){
     document.getElementById("fourth").style.display = 'none';
     document.getElementById($collab_id).style.display = 'block';
 }
-
 
 
 function showBio($person_id, $person_face){
